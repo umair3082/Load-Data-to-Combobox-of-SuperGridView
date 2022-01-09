f1f836cd8ab5c762618ff7b23c5b99866ffad198
Umair Rasheed <34655240+umair3082@users.noreply.github.com>
2022-01-09 13:35:13 +0500

    Update main.cs
    Make object of Class CommonCode

--- main.cs
+++ main.cs
@@ -1,6 +1,4 @@
+common_code cc = new common_code();
 #region Load CMB of SuperGridview
-
-            cc.CMB_in_SuperGridview_Load(superGridControl2, 0, @"SELECT distinct
-      [fee]
-  FROM [dbo].[fee]");
-            #endregion
+cc.CMB_in_SuperGridview_Load(superGridControl2, 0, @"SELECT distinct [fee] FROM [dbo].[fee]");
+#endregion
